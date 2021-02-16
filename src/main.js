0169dbf60c2ecfa9b7f6f2cea088481da09d764b
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-02-16 22:37:52 +0100

    feat: add basic drop hook with console log from Item prototype

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-"use strict"
+'use strict';
 
-Hooks.once("dropCanvasData", (data) => {
-    console.dir(data);
-})
+Hooks.once('init', () => {
+    hookItemDragHandlers();
+});
+
+function hookItemDragHandlers() {
+    const originalDragLeftDropHandler = Item.prototype._onDragLeftDrop;
+    Item.prototype._onDragLeftDrop = function (event) {
+        console.dir(event);
+        originalDragLeftDropHandler.call(this, event);
+    };
+}
