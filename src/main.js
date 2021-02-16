8ccff8e6269f2b7a832f39cb5540988fcf8faff9
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-02-16 22:56:59 +0100

    feat: add document drop hook with console log

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+Hooks.on('init', () => {
+    document.addEventListener("drop", (event) => {
+        console.dir(data);
+    });
+});
+
 Hooks.on('dropCanvasData', (data) => {
     console.dir(data);
 });
