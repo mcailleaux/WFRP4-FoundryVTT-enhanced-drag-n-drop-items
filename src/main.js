215500fae0010a9593099960e9508165fed9b23f
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-02-16 23:20:29 +0100

    feat: override onDrop ItemSheet

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 'use strict';
 
 Hooks.on('init', () => {
-    document.addEventListener('dragstart', (event) => {
+
+    const originalDropHandler = ItemSheet.prototype._onDrop;
+    ItemSheet.prototype._onDrop = async (event) => {
         console.dir(event);
-    });
-    document.addEventListener('drop', (event) => {
-        console.dir(event);
-    });
+        if (originalDropHandler != null) {
+            originalDropHandler.call(this, event);
+        }
+    };
+
+    // /** @override */
+    // async _onDrop(event) {
+    //     // Try to extract the data
+    //     let data;
+    //     try {
+    //         data = JSON.parse(event.dataTransfer.getData('text/plain'));
+    //     } catch (err) {
+    //         return false;
+    //     }
+    //     const actor = this.actor;
+    //     // Handle the drop with a Hooked function
+    //     const allowed = Hooks.call("dropActorSheetData", actor, this, data);
+    //     if ( allowed === false ) return;
+    //     // Handle different data types
+    //     switch ( data.type ) {
+    //         case "Item":
+    //             return this._onDropItem(event, data);
+    //         case "Actor":
+    //             return this._onDropActor(event, data);
+    //     }
+    // }
 });
