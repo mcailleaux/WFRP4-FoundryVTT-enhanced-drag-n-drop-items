554294a5a153e98a00d28f13fc228bfa17db60c5
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-02-16 23:33:33 +0100

    feat: override all dnd events

--- src/main.js
+++ src/main.js
@@ -2,8 +2,36 @@
 
 Hooks.on('init', () => {
 
+    const originalCanDragHandler = ItemSheet.prototype._canDragStart;
+    ItemSheet.prototype._canDragStart = async (event) => {
+        console.dir(this);
+        console.dir(event);
+        if (originalCanDragHandler != null) {
+            originalCanDragHandler.call(this, event);
+        }
+    };
+
+    const originalCanDropHandler = ItemSheet.prototype._canDragDrop;
+    ItemSheet.prototype._canDragDrop = async (event) => {
+        console.dir(this);
+        console.dir(event);
+        if (originalCanDropHandler != null) {
+            originalCanDropHandler.call(this, event);
+        }
+    };
+
+    const originalDragStartHandler = ItemSheet.prototype._onDragStart;
+    ItemSheet.prototype._onDragStart = async (event) => {
+        console.dir(this);
+        console.dir(event);
+        if (originalDragStartHandler != null) {
+            originalDragStartHandler.call(this, event);
+        }
+    };
+
     const originalDropHandler = ItemSheet.prototype._onDrop;
     ItemSheet.prototype._onDrop = async (event) => {
+        console.dir(this);
         console.dir(event);
         if (originalDropHandler != null) {
             originalDropHandler.call(this, event);
