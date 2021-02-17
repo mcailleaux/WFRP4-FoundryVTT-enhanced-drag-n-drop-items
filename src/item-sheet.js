dfcb6b99508fcb0b173f15d8c116df83240e388e
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-02-17 08:38:39 +0100

    feat: drop item on ItemSheet

---
+++ src/item-sheet.js
@@ -0,0 +1,51 @@
+export function initItemSheetDropHandler() {
+    const originalDropHandler = ItemSheet.prototype._onDrop;
+    ItemSheet.prototype._onDrop = async (event) => {
+        try {
+            const data = JSON.parse(event.dataTransfer.getData('text/plain'));
+            await dropData(data, this);
+        } catch (err) {
+            console.error(err);
+        }
+        if (originalDropHandler != null) {
+            originalDropHandler.call(this, event);
+        }
+    };
+
+    const defaultOption = ItemSheet.defaultOptions;
+    defaultOption.dragDrop.push({dragSelector: '.item-list .item', dropSelector: null});
+    Object.defineProperty(ItemSheet, 'defaultOptions', {
+        get: () => {
+            return defaultOption;
+        }
+    });
+}
+
+async function dropData(data, sheet) {
+    if (data == null) {
+        return;
+    }
+    switch (data.type) {
+        case 'Item':
+            const item = await Item.fromDropData(data);
+            await dropItem(item, sheet);
+            break;
+    }
+}
+
+async function dropItem(item, sheet) {
+    if (item == null) {
+        return;
+    }
+    console.dir(sheet);
+    console.dir(item);
+    // let value = target.value;
+    // if (value == null || value.trim() === '') {
+    //     value = item.name;
+    // } else {
+    //     value += ', ' + item.name;
+    // }
+    //
+    // target.value = value;
+
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+import {initItemSheetDropHandler} from './item-sheet';
+
 Hooks.on('init', () => {
-    initDropHandler();
+    initItemSheetDropHandler();
     // document.addEventListener('drop', async (event) => {
     //     if (
     //         event.target.nodeName !== 'INPUT' &&
@@ -22,54 +24,4 @@ Hooks.on('init', () => {
 
 });
 
-function initDropHandler() {
-    const originalDropHandler = ItemSheet.prototype._onDrop;
-    ItemSheet.prototype._onDrop = async function (event) {
-        try {
-            const data = JSON.parse(event.dataTransfer.getData('text/plain'));
-            await dropData(data);
-        } catch (err) {
-            console.error(err);
-        }
-        if (originalDropHandler != null) {
-            originalDropHandler.call(this, event);
-        }
-    };
 
-    const defaultOption = ItemSheet.defaultOptions;
-    defaultOption.dragDrop.push({dragSelector: '.item-list .item', dropSelector: null});
-    Object.defineProperty(ItemSheet, 'defaultOptions', {
-        get: () => {
-            return defaultOption;
-        }
-    });
-}
-
-async function dropData(data) {
-    if (data == null) {
-        return;
-    }
-    switch (data.type) {
-        case 'Item':
-            const item = await Item.fromDropData(data);
-            await dropItem(item);
-            break;
-    }
-}
-
-async function dropItem(item) {
-    if (item == null) {
-        return;
-    }
-    console.dir(this);
-    console.dir(item);
-    // let value = target.value;
-    // if (value == null || value.trim() === '') {
-    //     value = item.name;
-    // } else {
-    //     value += ', ' + item.name;
-    // }
-    //
-    // target.value = value;
-
-}
